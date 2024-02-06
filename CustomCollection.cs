d23650d8a361733edb2375d451308f355a388dad
wassem7 <wassemdarkwa@gmail.com>
2024-02-06 13:31:48 +0000

    Custom Collections Class

---
+++ CustomCollection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+
+namespace DSA1
+{
+    public class CustomCollection:CollectionBase
+    {
+
+        public void Add(object item)
+        {
+            InnerList.Add(item);
+        }
+
+        public void Remove(object item)
+        {
+            InnerList.Remove(item);
+        }
+
+        public new int Count()
+        {
+            return InnerList.Count;
+        }
+
+        public new void Clear()
+        {
+            InnerList.Clear();
+        }
+    }
+}
